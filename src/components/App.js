31d45604b1cce60c109d96b5c8e8c6722151015a
glebradnikov <glebr4dnikov@gmail.com>
2023-05-15 20:40:41 +0300

    fix: correct the link in App component

--- src/components/App.js
+++ src/components/App.js
@@ -175,7 +175,7 @@ export const App = () => {
           }
         />
         <Route
-          path='/adnrey-ekaterina-alisa'
+          path='/andrey-ekaterina-alisa'
           element={
             <Main
               adjective={obj.adjective1}

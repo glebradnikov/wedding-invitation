1a081b104117c2169aacb565ac73e73702b0662c
glebradnikov <glebr4dnikov@gmail.com>
2023-05-26 19:42:02 +0300

    refactor: change App.js

--- src/components/App.js
+++ src/components/App.js
@@ -305,6 +305,16 @@ export const App = () => {
           }
         />
         <Route
+          path='/vitaliy'
+          element={
+            <Main
+              adjective={obj.adjective3}
+              name='Виталий'
+              pronoun1={obj.pronoun2}
+            />
+          }
+        />
+        <Route
           path='/seymur-hursant'
           element={
             <Main
